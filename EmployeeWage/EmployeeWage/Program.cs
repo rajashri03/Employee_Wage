61566a4e5a8d361bb0d6c870826958637c4bfb7c
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-26 16:11:20 +0530

    UC1-Check Employee is Present or not

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -3,6 +3,17 @@ class Employeewage
 {
     public static void Main()
     {
-        Console.Write("*******Welcome to Employee Wage Computation Program********");
+        int Fulltime = 1;
+        Random random_num = new Random();
+        int checkemp = random_num.Next(0, 2);
+        if(checkemp == Fulltime)
+        {
+            Console.WriteLine("Employee Is Present");
+        }
+        else
+        {
+            Console.Write("Employee is Absent");
+        }
+
     }
 }
