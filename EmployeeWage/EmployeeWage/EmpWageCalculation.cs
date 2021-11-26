1e26601f2992c11c523e93342e9b5866c368e8f7
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-26 20:14:17 +0530

    UC4-Using Switch Case

--- EmployeeWage/EmployeeWage/EmpWageCalculation.cs
+++ EmployeeWage/EmployeeWage/EmpWageCalculation.cs
@@ -11,27 +11,27 @@ namespace EmployeeWage
         int PartTime = 1;
         int FullTime = 2;
         int PerHrRate = 20;
-        int emp_hr = 0;
-        int emp_wage = 0;
+        int Emp_Hr = 0;
+        int Emp_Wage = 0;
         Random random_num = new Random();
         public int EmpWage()
         {
-            int checkemp = random_num.Next(0, 2);
-            if (checkemp == PartTime)
+            int checkemp = random_num.Next(0, 3);
+            switch (checkemp)
             {
-                emp_hr = 8;
+                case 1:
+                    Emp_Hr = 4;
+                    break;
+                case 2:
+                    Emp_Hr = 8;
+                    break;
+                default:
+                    Emp_Hr = 0;
+                    break;
             }
-            else if (checkemp == FullTime)
-            {
-                emp_hr = 16;
-            }
-            else
-            {
-               emp_hr = 0;
-            }
-            emp_wage = emp_hr * PerHrRate;
-            Console.WriteLine("Part Time Employee Wage:-" + emp_wage);
-            return emp_wage;
+            Emp_Wage = Emp_Hr * PerHrRate;
+            Console.WriteLine("Part Time Employee Wage:-" + Emp_Wage);
+            return Emp_Wage;
         }
         
     }
--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -4,7 +4,7 @@ class Employeewage
 {
     public static void Main()
     {
-        Console.Write("*******Calculating Part Time Employee Wage:********\n");
+        Console.Write("*******Calculating Part Time Employee Wage using Switch Case:********\n");
         EmpWageCalculation empWageCalculation = new EmpWageCalculation();
         empWageCalculation.EmpWage();
         
