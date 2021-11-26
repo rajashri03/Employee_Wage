2a44a8f51c9ce7291f6759dcc88eaf045ad0aec7
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-26 19:53:40 +0530

    UC3-Calculating Part Time Wage

---
+++ EmployeeWage/EmployeeWage/EmpWageCalculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpWageCalculation
+    {
+        int PartTime = 1;
+        int FullTime = 2;
+        int PerHrRate = 20;
+        int emp_hr = 0;
+        int emp_wage = 0;
+        Random random_num = new Random();
+        public int EmpWage()
+        {
+            int checkemp = random_num.Next(0, 2);
+            if (checkemp == PartTime)
+            {
+                emp_hr = 8;
+            }
+            else if (checkemp == FullTime)
+            {
+                emp_hr = 16;
+            }
+            else
+            {
+               emp_hr = 0;
+            }
+            emp_wage = emp_hr * PerHrRate;
+            Console.WriteLine("Part Time Employee Wage:-" + emp_wage);
+            return emp_wage;
+        }
+        
+    }
+}
--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,25 +1,12 @@
 ﻿using System;
+using EmployeeWage;
 class Employeewage
 {
     public static void Main()
     {
-        int Fulltime = 1;
-        int Per_hr_rate = 20;
-        int emp_hr = 0;
-        int emp_wage = 0;
-        Random random_num = new Random();
-        int checkemp = random_num.Next(0, 2);
-        if (checkemp == Fulltime)
-        {
-            emp_hr = 8;
-        }
-        else
-        {
-            emp_hr = 0;
-        }
-        emp_wage = emp_hr * Per_hr_rate;
-        Console.WriteLine("Employee Wage:-" + emp_wage);
-
-
+        Console.Write("*******Calculating Part Time Employee Wage:********\n");
+        EmpWageCalculation empWageCalculation = new EmpWageCalculation();
+        empWageCalculation.EmpWage();
+        
     }
 }
