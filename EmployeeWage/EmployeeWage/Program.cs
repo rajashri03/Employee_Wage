d4f04b6d072c8276ecc191f65e908304ca952513
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-26 16:45:38 +0530

    UC2-Calculate Employee Wage

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -4,16 +4,22 @@ class Employeewage
     public static void Main()
     {
         int Fulltime = 1;
+        int Per_hr_rate = 20;
+        int emp_hr = 0;
+        int emp_wage = 0;
         Random random_num = new Random();
         int checkemp = random_num.Next(0, 2);
-        if(checkemp == Fulltime)
+        if (checkemp == Fulltime)
         {
-            Console.WriteLine("Employee Is Present");
+            emp_hr = 8;
         }
         else
         {
-            Console.Write("Employee is Absent");
+            emp_hr = 0;
         }
+        emp_wage = emp_hr * Per_hr_rate;
+        Console.WriteLine("Employee Wage:-" + emp_wage);
+
 
     }
 }
